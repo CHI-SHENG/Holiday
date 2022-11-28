ce6db9278d39440bf4e6a53d38d1b7f132e2ccde
齊聲 <raynboy50703@gmail.com>
2022-11-29 04:39:33 +0800

    deploy

---
+++ js/about.cb77db4b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkholiday"]=self["webpackChunkholiday"]||[]).push([[443],{1272:function(n,t,u){u.r(t),u.d(t,{default:function(){return r}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],l=u(1001),e={},i=(0,l.Z)(e,s,a,!1,null,null,null),r=i.exports}}]);
+//# sourceMappingURL=about.cb77db4b.js.map
